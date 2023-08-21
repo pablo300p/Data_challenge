72ecc8d18d60788cdfcfb13fdfd9b5b1c2c459a6
pablo300p <80368380+pablo300p@users.noreply.github.com>
2023-08-20 21:13:51 -0600

    Update app.py
    Sql usage to make the tables and insert the data

--- app.py
+++ app.py
@@ -1,73 +1,73 @@
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource
+from flask import Flask, request
+import psycopg2 
 import os
+import pandas as pd
+
+
+CREATE_HIRED_EMPLOYEES_TABLE = """CREATE TABLE IF NOT EXISTS Hired_Employees (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, datetime VARCHAR(255) NOT NULL, department_id INTEGER NOT NULL, job_id INTEGER NOT NULL);"""
+
+CREATE_DEPARTMENTS_TABLE = """ CREATE TABLE IF NOT EXISTS Departments (id SERIAL PRIMARY KEY, department VARCHAR(255) NOT NULL);"""
+
+CREATE_JOBS_TABLE = """ CREATE TABLE IF NOT EXISTS Jobs (id SERIAL PRIMARY KEY, job VARCHAR(255) NOT NULL);"""
+
+
+INSERT_EMPLOYEES = """INSERT INTO Hired_Employees (name, datetime, department_id, job_id) VALUES (%s, %s, %s, %s);"""
+
+INSERT_DEPARTMENTS = """INSERT INTO Departments (department) VALUES (%s);"""
+
+INSERT_JOBS = """INSERT INTO Jobs (job) VALUES (%s);"""
+
+#EMPLOYEES_DETAILS = """SELECT department, job, CASE FROM Hired_Employees;"""
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-db = SQLAlchemy(app)
 
-class Employee(db.Model):
-  __tablename__ = 'Hired_Employees'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), unique=True, nullable=False)
-  datetime = db.Column(db.String(120), unique=True, nullable=False)
-  department_id = db.Column(db.Integer, nullable=False)
-  job_id = db.Column(db.Integer, nullable=False)
-
-  def __init__(self, name, datetime, department_id, job_id):
-    self.name = name
-    self.datetime = datetime
-    self.department_id = department_id
-    self.job_id = job_id
+url = 'postgresql://postgres:postgress@db:5432/test'
+connection = psycopg2.connect(url)
 
 
-db.create_all()
+@app.route("/api/employee", methods=['POST'])
+def create_employee():
+    data = request.get_json()
+    name = data['name']
+    datetime = data['datetime']
+    department_id = data['department_id']
+    job_id = data['job_id']
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_HIRED_EMPLOYEES_TABLE)
+            # Execute many accept a list of rows allowing us to insert multiple rows at once
+            cursor.executemany(INSERT_EMPLOYEES, [
+                (name, datetime, department_id, job_id)
+            ])
+            name = cursor.fetchone()[0]
+    return {'name': name}, 201
 
-class EmployeeResource(Resource):
-#Get one item in the database
-  @app.route('/employee/<id>', methods=['GET'])
-  def get_item(id):
-    item = Employee.query.get(id)
-    del item.__dict__['_sa_instance_state']
-    return jsonify(item.__dict__)
+@app.route("/api/department", methods=['POST'])
+def create_department():
+    data = request.get_json()
+    department = data['department']
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_DEPARTMENTS_TABLE)
+            cursor.executemany(INSERT_DEPARTMENTS, [
+                (department)
+            ])
+            name = cursor.fetchone()[0]
+    return {'name': name}, 201
 
-# Get all items in the database
-  @app.route('/employees', methods=['GET'])
-  def get_items():
-    items = []
-    for item in db.session.query(Employee).all():
-      del item.__dict__['_sa_instance_state']
-      items.append(item.__dict__)
-    return jsonify(items)
-
-#Create a new item in the database
-  @app.route('/employees', methods=['POST'])
-  def create_employee():
-    try:
-      body = request.get_json()
-      new_item = Employee(name=body['name'], datetime=body['datetime'], department_id=body['department_id'], job_id=body['job_id'])
-      db.session.add(new_item)
-      db.session.commit()
-      return "item created"
-    except Exception:
-      return "Item could not be created" 
-        
-#To update an existing enployee in the database
-  @app.route('/Employee/<id>', methods=['PUT'])
-  def update_item(id):
-    body = request.get_json()
-    db.session.query(Employee).filter_by(id=id).update(
-      dict(name=body['name'], datetime=body['datetime'], department_id=body['department_id'], job_id=body['job_id']))
-    db.session.commit()
-    return "item updated"
-
-#To delete an existing employee in the database
-  @app.route('/employee/<id>', methods=['DELETE'])
-  def delete_item(id):
-    db.session.query(Employee).filter_by(id=id).delete()
-    db.session.commit()
-    return "item deleted"
+@app.route("/api/job", methods=['POST'])
+def create_job():
+    data = request.get_json()
+    job = data['job']
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_JOBS_TABLE)
+            cursor.executemany(INSERT_JOBS, [
+                (job)
+            ])
+            name = cursor.fetchone()[0]
+    return {'name': name}, 201
 
 
 if __name__ == "__main__":
